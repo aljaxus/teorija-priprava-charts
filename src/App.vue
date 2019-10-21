5afb9674d8d97a1f3d6ad622ff07e8231762811a
aljaxus <me@aljaxus.eu>
2019-10-21 22:17:51 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <apexchart type=area height=500 :options="options" :series="series" />
+    PASTE THE WHOLE PAGE TEXT FROM http://teorija-priprava.gov.si/ IN THIS INPUT
+    <textarea v-model="text" name="text" cols="30" rows="3" ref="text" @focus="$event.target.select()"></textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => ({
+    text: '',
+    months: {
+      januar: 'january',
+      februar: 'february',
+      marec: 'march',
+      april: 'april',
+      maj: 'may',
+      junij: 'june',
+      julij: 'july',
+      avgust: 'august',
+      september: 'september',
+      oktober: 'october',
+      november: 'november',
+      december: 'december',
+    },
+    rxp: /([0-9]{1,2}\. ([a-z]+) [0-9]{1,4}, [0-9:]{1,5})[^0-9]+([0-9]{1,2},[0-9]{1,2}) 	([0-9]{1,2},[0-9]{1,2})/,
+  }),
+  computed: {
+    strings () { return this.text.match(new RegExp(this.rxp, 'g')) || [] },
+    times () { return this.strings.map(el => {
+      const time = el.match(new RegExp(this.rxp))
+      const { months } = this
+
+      const str = time[1].replace(time[2], months[time[2]]).replace('.', '')
+      return new Date(str).toISOString()
+    }) },
+    points () { return this.strings.map(el => el.match(new RegExp(this.rxp))[3]) },
+    per100 () { return this.strings.map(el => {
+      const str = (el.match(new RegExp(this.rxp))[4]).replace(',', '.')
+      return Math.floor(Number(str) * 10)
+    })},
+    average () {
+      let total = 0
+
+      return this.per100.map((el, i) => {
+        total = total + el
+        return Math.floor(total / (i + 1))
+      })
+    },
+    series () {
+      return [
+        { name: 'Points', data: this.points },
+        { name: 'Percent', data: this.per100 },
+        { name: 'Average', data: this.average }
+      ]
+    },
+    options () {
+      const { times } = this
+      return {
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: times,
+        },
+        yaxis: {
+          max: 100
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        }
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApexCharts from 'vue-apexcharts'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   render: h => h(App),
